08926d94e19720bca2d781d09d5e96eb052122d0
HineRepository <tolotti.f@intergeanordest.it>
2025-01-30 15:33:21 +0100

    integrata api veicoli

---
+++ DatabaseMapping/Views/VeicoloMap.py
@@ -0,0 +1,54 @@
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Veicolo(db.Model):
+    __tablename__ = 'geve_veicoli'
+    __table_args__ = {'schema': 'bi'}
+
+    dim_veicolo: Mapped[str] = mapped_column(primary_key=True)
+    azienda_veicolo: Mapped[str] = mapped_column()
+    id_gesionale_veicolo: Mapped[int] = mapped_column()
+    telaio_veicolo: Mapped[str] = mapped_column()
+    targa_veicolo: Mapped[str] = mapped_column()
+    dt_immatricolazione_veicolo: Mapped[date] = mapped_column()
+    dt_arrivo_veicolo: Mapped[date] = mapped_column()
+    dt_uscita_veicolo: Mapped[date] = mapped_column()
+    gruppo_marca_veicolo: Mapped[str] = mapped_column()
+    modello_veicolo: Mapped[str] = mapped_column()
+    veicolo: Mapped[str] = mapped_column()
+    gruppo_tipo_veicolo: Mapped[str] = mapped_column()
+    alimentazione_veicolo: Mapped[str] = mapped_column()
+    nuovo_usato_veicolo: Mapped[str] = mapped_column()
+    status_veicolo: Mapped[str] = mapped_column()
+    ubicazione_attuale_veicolo: Mapped[str] = mapped_column()
+    km_percorsi_veicolo: Mapped[int] = mapped_column()
+    linea_veicolo: Mapped[str] = mapped_column()
+
+    def toJson(self):
+        return {"id": self.dim_veicolo,
+            "azienda": self.azienda_veicolo,
+            "id_veicolo_gestionale": self.id_gesionale_veicolo,
+            "vin": self.telaio_veicolo,
+            "targa": self.targa_veicolo,
+            "data_immatricolazione": self.dt_immatricolazione_veicolo.isoformat() if self.dt_immatricolazione_veicolo else None,
+            "data_arrivo": self.dt_arrivo_veicolo.isoformat() if self.dt_arrivo_veicolo else None,
+            "data_uscita": self.dt_uscita_veicolo.isoformat() if self.dt_uscita_veicolo else None,
+            "marca": self.gruppo_marca_veicolo,
+            "modello": self.modello_veicolo,
+            "veicolo": self.veicolo,
+            "tipo_veicolo": self.gruppo_tipo_veicolo,
+            "alimentazione": self.alimentazione_veicolo,
+            "nuovo_usato": self.nuovo_usato_veicolo,
+            "status": self.status_veicolo,
+            "ubicazione_ultima": self.ubicazione_attuale_veicolo,
+            "km_percorsi": self.km_percorsi_veicolo,
+            "linea": self.linea_veicolo
+        }
--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from DatabaseMapping.Views.VeicoloMap import Veicolo
 from DatabaseMapping.Views.AziendaMap import Azienda
 from Config import Config
 
@@ -18,7 +19,8 @@ app.config.from_object(Config)
 
 # initialize the app with the extension
 db.init_app(app)
-
+'''
+#==================================================AZIENDA===============================================
 # esempio chiamata
 #http://127.0.0.1:5000/aziende
 @app.route("/aziende", methods=['GET'])
@@ -27,15 +29,36 @@ def aziende_list():
     aziende_list = [Azienda.toJson(azienda) for azienda in aziende]
     return jsonify(aziende_list)
 
-# Endpoint per ottenere un singolo elemento per ID
 # esempio chiamata
-#http://127.0.0.1:5000/aziende/Id/AT
-@app.route('/aziende/Id/<string:azienda_id>', methods=['GET'])
+#http://127.0.0.1:5000/aziende/id/AT
+@app.route('/aziende/id/<string:azienda_id>', methods=['GET'])
 def azienda_By_Id(azienda_id):
     aziende = db.session.execute(db.select(Azienda).where(Azienda.Id == azienda_id)).scalars() 
     aziende_list = [Azienda.toJson(azienda) for azienda in aziende]
     return jsonify(aziende_list)
-    
+
+'''
+
+
+#==================================================VEICOLO===============================================
+# esempio chiamata
+#http://127.0.0.1:5000/veicoli/vin/ZFA16900001091537
+@app.route('/veicoli/vin/<string:vin>', methods=['GET'])
+def veicolo_By_Vin(vin):
+    result = db.session.execute(db.select(Veicolo).where(Veicolo.telaio_veicolo == vin)).scalars() 
+    resultToJson = [Veicolo.toJson(item) for item in result]
+    return jsonify(resultToJson)
+
+# esempio chiamata
+#http://127.0.0.1:5000/veicoli/targa/EX858RN
+@app.route('/veicoli/targa/<string:targa>', methods=['GET'])
+def veicolo_By_targa(targa):
+    result = db.session.execute(db.select(Veicolo).where(Veicolo.targa_veicolo == targa)).scalars() 
+    resultToJson = [Veicolo.toJson(item) for item in result]
+    return jsonify(resultToJson)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
